7d313c382c5b5275d4e5a97f8dfcd48416d0ebc7
headcruser <headcruser@gmail.com>
2022-12-11 13:59:47 -0600

    Se modifica el mensaje Acerca de

--- resources/js/Pages/About.vue
+++ resources/js/Pages/About.vue
@@ -1,18 +1,24 @@
 <script setup>
-    import {
-        MDBContainer,
-        MDBCard,
-        MDBCardHeader,
-        MDBCardBody,
-    } from "mdb-vue-ui-kit";
-
-    import { ref } from "vue";
-
-    import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 </script>
 
 <template>
-    <AuthenticatedLayout>
-       <h2>About Us</h2>
-    </AuthenticatedLayout>
+  <AuthenticatedLayout>
+    <section class="mb-10" style="background-color: #f3f7fa">
+      <div class="container py-5">
+        <div class="row my-md-4 gx-md-5">
+          <div class="col-12">
+            <h4 class="text-uppercase fw-bold ls-tighter mb-3">Daniel Martinez Sierra</h4>
+            <p class="lead mb-4">
+              Proyecto <strong>Laravel 9 + MDB + vite  + Breeze + vuejs</strong>, template inicial para trabajar en proyectos
+              con la configuración que utilizo en mi dia a dia
+            </p>
+
+            <span class="lead">2022</span>
+            <h6 class="mb-0">Contacto: <a href="mailto:headcruser@gmial.com">headcruser@gmail.com</a></h6>
+          </div>
+        </div>
+      </div>
+    </section>
+  </AuthenticatedLayout>
 </template>
