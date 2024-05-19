07f1cf66eb40fa01381debdd13c8a073078acab9
headcruser <headcruser@gmail.com>
2024-05-18 19:52:39 -0600

    Seeders: Correccion en productos
    - Se corrige valor por defecto de los seeders

--- database/seeders/ProductoSeeder.php
+++ database/seeders/ProductoSeeder.php
@@ -20,30 +20,35 @@ class ProductoSeeder extends Seeder
                     'codigo'        => 'VN',
                     'descripcion'   => 'Vaso con diferentes gomitas',
                     'precio'        => 25,
+                    'visible'       => true,
                 ],
                 [
                     'nombre'        => 'VASO LOCO',
                     'codigo'        => 'VL',
                     'descripcion'   => 'Vaso con gomitas, chicolines, skincles rellenos',
                     'precio'        => 35,
+                    'visible'       => true,
                 ],
                 [
                     'nombre'        => 'VASO GUMMY',
                     'codigo'        => 'VL',
                     'descripcion'   => 'Vaso con solo con un tipo de gomitas',
                     'precio'        => 30,
+                    'visible'       => true,
                 ],
                 [
                     'nombre'        => 'VASO SKITTLES',
                     'codigo'        => 'VS',
                     'descripcion'   => 'Vaso con solo Skittles',
                     'precio'        => 35,
+                    'visible'       => true,
                 ],
                 [
                     'nombre'        => 'VASO CACAHUATE',
                     'codigo'        => 'VC',
                     'descripcion'   => 'Vaso con cacahuates japoneses',
                     'precio'        => 35,
+                    'visible'       => true,
                 ],
             ]);
         }
